92f2206c10d8802dc02a1d000882fe536367c500
Vrushal Talegaonkar <vrushal.hi@gmail.com>
2015-06-18 19:13:01 +0530

    sync

---
+++ TestApp-MVC-Git-Azure/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestApp_MVC_Git_Azure.MvcApplication" Language="C#" %>
---
+++ TestApp-MVC-Git-Azure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestApp_MVC_Git_Azure.Startup))]
+namespace TestApp_MVC_Git_Azure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
